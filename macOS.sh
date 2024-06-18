17be65292230a261d95b732eebfc991bd59d739a
wlstory <wlstory@gmail.com>
2024-06-18 10:11:24 -0500

    Update macOS.sh
    moved first block of MacOS settings from brew.sh to macOS.sh

--- macOS.sh
+++ macOS.sh
@@ -28,6 +28,48 @@ read
 
 defaults write com.apple.finder QLInlinePreviewMinimumSupportedSize -int 512 # Disable in-icon previews
 
+# Configure Finder
+echo "Configuring Finder ----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+defaults write com.apple.finder AppleShowAllFiles -bool true
+defaults write com.apple.finder ShowStatusBar -bool true
+defaults write com.apple.finder ShowPathbar -bool true
+defaults write com.apple.finder AppleShowAllExtensions -bool true
+defaults write com.apple.finder FXEnableExtensionChangeWarning -bool true
+defaults write com.apple.finder FXEnableRemoveFromICloudDriveWarning -bool true
+defaults write com.apple.finder FXInfoPanesExpanded -dict General -bool true OpenWith -bool true Privileges -bool true
+defaults write com.apple.finder _FXSortFoldersFirst -bool true
+
+# Automatically open a new Finder window when a volume is mounted
+defaults write com.apple.frameworks.diskimages auto-open-ro-root -bool false
+defaults write com.apple.frameworks.diskimages auto-open-rw-root -bool false
+defaults write com.apple.finder OpenWindowForNewRemovableDisk    -bool false
+
+# Restart Finder
+killall Finder
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>---- Finder Configuration Complete"
+
+# Configure Safari
+echo "Configuring Safari ----<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
+defaults write com.apple.Safari IncludeInternalDebugMenu -bool true
+defaults write com.apple.Safari IncludeDevelopMenu -bool true
+defaults write com.apple.Safari WebKitDeveloperExtrasEnabledPreferenceKey -bool true
+# Safari opens with: last session
+defaults write com.apple.Safari AlwaysRestoreSessionAtLaunch -bool true
+# disable safari auto open files
+defaults write com.apple.Safari AutoOpenSafeDownloads -bool false
+# Disable saving passwords - avoid issues with external password managers)
+defaults write com.apple.Safari AutoFillPasswords -bool false            
+# Disable auto filling Credit Cards
+defaults write com.apple.Safari AutoFillCreditCardData -bool false 
+defaults write NSGlobalDomain WebKitDeveloperExtras -bool true      
+killall Safari
+echo ">>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>>---- Safari Configuration Complete"
+
+# Configure Dock
+defaults write com.apple.dock autohide -bool true
+defaults write com.apple.dock show-recents -bool false
+defaults write com.apple.dock tilesize -int 36
+
 # Adding items to the doc and setting hot corners requires a Dock restart
 killall Dock
 killall Finder
